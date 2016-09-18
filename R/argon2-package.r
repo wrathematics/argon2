47430ad5406748df9ca09cbbd07905d50f64d9d7
wrathematics <wrathematics@gmail.com>
2016-09-18 13:39:09 -0400

    add R package boilerplate

---
+++ R/argon2-package.r
@@ -0,0 +1,12 @@
+#' argon2
+#'
+#' TODO
+#' 
+#' @references Project URL: \url{https://github.com/wrathematics/argon2}
+#' @author Drew Schmidt
+#' 
+#' @name argon2-package
+#' 
+#' @docType package
+#' @keywords package
+NULL
