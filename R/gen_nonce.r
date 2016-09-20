73046029f712e7ae7531f7a2ae7bc7801f28e3d4
wrathematics <wrathematics@gmail.com>
2016-09-20 09:13:10 -0400

    I can count

--- R/gen_nonce.r
+++ R/gen_nonce.r
@@ -3,14 +3,14 @@
 #' Generates a random raw (unsigned char*) vector.
 #' 
 #' @param length
-#' The number of items to return. Default is 64 items, which
+#' The number of items to return. Default is 128 items, which
 #' is 512 hex bits.
 #' 
 #' @return
 #' A random raw vector.
 #' 
 #' @export
-gen_nonce <- function(length=64)
+gen_nonce <- function(length=128)
 {
   check.is.posint(length)
   
