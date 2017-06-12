f91db5efcae5fcf3dfba2fc80b11307461db82aa
wrathematics <wrathematics@gmail.com>
2017-06-12 07:52:27 -0400

    improve raw_as_char() test readability

--- tests/raw_as_char.R
+++ tests/raw_as_char.R
@@ -1,12 +1,26 @@
 library(argon2)
+
+compare = function(test, truth) stopifnot(identical(truth, test))
+
 x = charToRaw("asdfZ")
 
-truth <- capture.output(print(x))
-truth <- substr(truth, 5, nchar(truth))
 
-stopifnot(identical(truth, raw_as_char(x, FALSE, TRUE)))
-stopifnot(identical(toupper(truth), raw_as_char(x, TRUE, TRUE)))
 
-truth <- paste0(unlist(strsplit(truth, split=" ")), collapse="")
-stopifnot(identical(truth, raw_as_char(x, FALSE, FALSE)))
-stopifnot(identical(toupper(truth), raw_as_char(x, TRUE, FALSE)))
+### spaces = TRUE
+truth = capture.output(print(x))
+truth = substr(truth, 5, nchar(truth))
+test = raw_as_char(x, upper=FALSE, spaces=TRUE)
+compare(test, truth)
+
+test = raw_as_char(x, upper=TRUE, spaces=TRUE)
+compare(test, toupper(truth))
+
+
+
+### spaces = FALSE
+truth = paste0(unlist(strsplit(truth, split=" ")), collapse="")
+test = raw_as_char(x, upper=FALSE, spaces=FALSE)
+compare(test, truth)
+
+test = raw_as_char(x, upper=TRUE, spaces=FALSE)
+compare(test, toupper(truth))
